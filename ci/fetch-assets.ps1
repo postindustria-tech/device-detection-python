92ac9dec4ec5200cff9c657c1896ce8d1c2cdd9a
Alexander Popel <alexander.popel@postindustria.com>
2023-05-30 15:59:54 +0300

    Improve fetch-assets caching

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -16,23 +16,25 @@ $ErrorActionPreference = 'Stop'
 $assets = New-Item -ItemType Directory -Path assets -Force
 $deviceDetectionData = "$RepoName/fiftyone_devicedetection_onpremise/fiftyone_devicedetection_onpremise/device-detection-cxx/device-detection-data"
 
-$urls = @{
-    "51Degrees-LiteV4.1.hash" = "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash"
-    "20000 Evidence Records.yml" = "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20Evidence%20Records.yml"
-    "20000 User Agents.csv" = "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20User%20Agents.csv"
-    "51Degrees-Tac.zip" = "https://storage.googleapis.com/51degrees-assets/$DeviceDetection/51Degrees-Tac.zip"
+$downloads = @{
+    "TAC-HashV41.hash" = {
+        ./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
+        Move-Item -Path $RepoName/$file -Destination $assets
+    }
+    "51Degrees-LiteV4.1.hash" = {Invoke-WebRequest -Uri "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash" -OutFile $assets/$file}
+    "20000 Evidence Records.yml" = {Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20Evidence%20Records.yml" -OutFile $assets/$file}
+    "20000 User Agents.csv" = {Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20User%20Agents.csv" -OutFile $assets/$file}
+    "51Degrees.csv" = {
+        Invoke-WebRequest -Uri "https://storage.googleapis.com/51degrees-assets/$DeviceDetection/51Degrees-Tac.zip" -OutFile 51Degrees-Tac.zip
+        Expand-Archive -Path 51Degrees-Tac.zip
+        Get-Content -TotalCount 1 51Degrees-Tac/51Degrees-Tac-All.csv | Out-File $assets/$file # We only need a header
+    }
 }
 
-if (!(Test-Path "$assets/TAC-HashV41.hash")) {
-    ./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
-    Move-Item -Path $RepoName/TAC-HashV41.hash -Destination $assets
-} else {
-    Write-Output "'$assets/TAC-HashV41.hash' exists, skipping download"
-}
-
-foreach ($file in $urls.Keys) {
-    if (!(Test-Path $file)) {
-        Invoke-WebRequest -Uri $urls[$file] -OutFile "$assets/$file"
+foreach ($file in $downloads.Keys) {
+    if (!(Test-Path $assets/$file)) {
+        Write-Output "Downloading $file"
+        Invoke-Command -ScriptBlock $downloads[$file]
     } else {
         Write-Output "'$file' exists, skipping download"
     }
@@ -46,8 +48,4 @@ Copy-Item -Path $assets/TAC-HashV41.hash -Destination $deviceDetectionData/Enter
 New-Item -ItemType SymbolicLink -Force -Target "$assets/51Degrees-LiteV4.1.hash" -Path "$deviceDetectionData/51Degrees-LiteV4.1.hash"
 New-Item -ItemType SymbolicLink -Force -Target "$assets/20000 Evidence Records.yml" -Path "$deviceDetectionData/20000 Evidence Records.yml"
 New-Item -ItemType SymbolicLink -Force -Target "$assets/20000 User Agents.csv" -Path "$deviceDetectionData/20000 User Agents.csv"
-
-# This one needs extraction (10GB file inside, can't cache it uncompressed)
-Write-Output "Extracting '51Degrees-Tac.zip'"
-Expand-Archive -Path $assets/51Degrees-Tac.zip -DestinationPath $RepoName/fiftyone_devicedetection_cloud/tests/
-Rename-Item -Path $RepoName/fiftyone_devicedetection_cloud/tests/51Degrees-Tac-All.csv -NewName 51Degrees.csv
+New-Item -ItemType SymbolicLink -Force -Target "$assets/51Degrees.csv" -Path "$RepoName/fiftyone_devicedetection_cloud/tests/51Degrees.csv"
