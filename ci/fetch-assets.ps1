39fed01f6be52d91e9f705a2696873c85f8eee50
Alexander Popel <alexander.popel@postindustria.com>
2023-06-06 13:26:09 +0300

    Update assets urls

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -21,9 +21,9 @@ $downloads = @{
         ./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
         Move-Item -Path $RepoName/$file -Destination $assets
     }
-    "51Degrees-LiteV4.1.hash" = {Invoke-WebRequest -Uri "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash" -OutFile $assets/$file}
-    "20000 Evidence Records.yml" = {Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20Evidence%20Records.yml" -OutFile $assets/$file}
-    "20000 User Agents.csv" = {Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20User%20Agents.csv" -OutFile $assets/$file}
+    "51Degrees-LiteV4.1.hash" = {Invoke-WebRequest -Uri "https://storage.googleapis.com/51degrees-assets/$DeviceDetection/51Degrees-LiteV4.1.hash" -OutFile $assets/$file}
+    "20000 Evidence Records.yml" = {Invoke-WebRequest -Uri "https://storage.googleapis.com/51degrees-assets/$DeviceDetection/20000%20Evidence%20Records.yml" -OutFile $assets/$file}
+    "20000 User Agents.csv" = {Invoke-WebRequest -Uri "https://storage.googleapis.com/51degrees-assets/$DeviceDetection/20000%20User%20Agents.csv" -OutFile $assets/$file}
     "51Degrees.csv" = {
         Invoke-WebRequest -Uri "https://storage.googleapis.com/51degrees-assets/$DeviceDetection/51Degrees-Tac.zip" -OutFile 51Degrees-Tac.zip
         Expand-Archive -Path 51Degrees-Tac.zip
