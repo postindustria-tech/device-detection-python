a90cbad56c601e3d58f057be2b57134567298d9d
Alexander Popel <alexander.popel@postindustria.com>
2023-06-06 12:13:56 +0300

    Correctly name performance results file

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -1,17 +1,19 @@
 param (
     [Parameter(Mandatory=$true)]
-    [string]$RepoName
+    [string]$RepoName,
+    [Parameter(Mandatory=$true)]
+    [string]$Name
 )
 
 $perfSummary = New-Item -ItemType directory -Path $RepoName/test-results/performance-summary -Force
 
 Push-Location $RepoName
 try {
-	Write-Output "Running performance tests"
-	python fiftyone_devicedetection_examples/performance-tests/test_performance.py ../assets/51Degrees-LiteV4.1.hash ../assets/"20000 User Agents.csv" || $(throw "benchmark failed with code: $LASTEXITCODE")
-	Get-Content -Path performance_test_summary.json
-	Move-Item -Path performance_test_summary.json -Destination $perfSummary/results_$RepoName.json || $(throw "failed to move summary")
-	Write-Output "OK"
+    Write-Output "Running performance tests"
+    python fiftyone_devicedetection_examples/performance-tests/test_performance.py ../assets/51Degrees-LiteV4.1.hash ../assets/"20000 User Agents.csv" || $(throw "benchmark failed with code: $LASTEXITCODE")
+    Get-Content -Path performance_test_summary.json
+    Move-Item -Path performance_test_summary.json -Destination $perfSummary/results_$Name.json || $(throw "failed to move summary")
+    Write-Output "OK"
 } finally {
-	Pop-Location
+    Pop-Location
 }
