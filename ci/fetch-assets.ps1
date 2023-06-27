3ce0dcb11137b86f43fca63248d3bed983e8c48a
ben51degrees <ben@51degrees.com>
2023-06-27 15:06:18 +0100

    Update fetch-assets.ps1

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -27,10 +27,8 @@ $downloads = @{
     "20000 User Agents.csv" = {Invoke-WebRequest -Uri "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20User%20Agents.csv" -OutFile $assets/$file}
     "51Degrees.csv" = {
         ./steps/fetch-csv-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
-        Move-Item -Path $RepoName/51Degrees-TacV3.4.trie.zip -Destination 51Degrees-TacV3.4.trie.zip
-        Expand-Archive -Path 51Degrees-TacV3.4.trie.zip
-        Get-Content -TotalCount 1 51Degrees-TacV3.4.trie/51Degrees-Tac-All.csv | Out-File $assets/$file # We only need a header
-        Remove-Item -Path 51Degrees-TacV3.4.trie.zip, 51Degrees-TacV3.4.trie/51Degrees-Tac-All.csv
+        Get-Content -TotalCount 1 $RepoName/51Degrees-TacV3.4.trie/51Degrees-Tac-All.csv | Out-File $assets/$file # We only need a header
+        Remove-Item -Path $RepoName/51Degrees-TacV3.4.trie.zip, $RepoName/51Degrees-TacV3.4.trie/51Degrees-Tac-All.csv
     }
 }
 
