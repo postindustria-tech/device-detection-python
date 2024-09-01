23049344280f960c49ab99df0d6118dc4b1c27d6
Automation51D <51DCI@51Degrees.com>
2024-09-01 00:24:47 +0000

    REF: Updated copyright.

--- fiftyone_devicedetection_shared/src/fiftyone_devicedetection_shared/example_constants.py
+++ fiftyone_devicedetection_shared/src/fiftyone_devicedetection_shared/example_constants.py
@@ -18,7 +18,7 @@
 # including the attribution notice(s) required under Article 5 of the EUPL
 # in the end user terms of the application under an appropriate heading,
 # such notice(s) shall fulfill the requirements of that article.
-# ********************************************************************* 
+# *********************************************************************
 EVIDENCE_VALUES = [
         # A User-Agent from a mobile device.
         { "header.user-agent":
